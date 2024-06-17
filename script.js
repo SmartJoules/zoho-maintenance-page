edd741b36f7a37e04328232b7ebc9e536f72de8d
Smartjoules <arun.kumar@smartjoules.in>
2024-06-17 17:24:25 +0530

    Debug

--- script.js
+++ script.js
@@ -342,7 +342,7 @@ ZOHO.CREATOR.init()
                 const flag_resp = document.querySelector(`#flag${i}`).checked;
                 const resp_option = document.querySelector(`#response-type${i}`).textContent;
                 const remark_output = document.querySelector(`#remark${i}`).value || "";
-
+                alert ("Its Fine");
                 let choice_id = "";
                 if (resp_option === "Multiple Choice") {
                     try {
